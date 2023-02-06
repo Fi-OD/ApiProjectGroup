ce973ecf85a5d990def617dee64635fe3775055e
FOD <fionnuala.k.odonnell@gmail.com>
2023-02-06 23:54:05 +0000

    updated the JS  psudo code and started work on the script for a random recipe generator button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+
+
+/////////////////THE FOLLOWING CODE IS FOR THE RECIPE SEARCH SECTION/////////////////////
+
 const options = {
 	method: 'GET',
 	headers: {
@@ -6,58 +10,160 @@ const options = {
 	}
 };
 
-
-
 //use and event listener to take the inputted
 
 
-let inputField =document.querySelector("#search-input")
+let inputField = document.querySelector("#search-input")
 let recipeName = "";
 let searchBtn = document.querySelector("#search-button");
 // this function captures the inputted recipe name and stores it in a variable called recipe name
 
-searchBtn.addEventListener("click",function(event){
-    event.preventDefault();
-    recipeName = inputField.value;
-    console.log(recipeName);
+searchBtn.addEventListener("click", function (event) {
+	event.preventDefault();
+	recipeName = inputField.value;
+	console.log(recipeName);
 
-// this code request a recipe from the tasty API that matches the inputed variable called recipeName	
+	// this code request a recipe from the tasty API that matches the inputed variable called recipeName	
 	fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=1&q=' + recipeName, options)
-	.then(response => response.json())
-	.then(function(recipeResponse){
-		console.log(recipeResponse)
+		.then(response => response.json())
+		.then(function (recipeResponse) {
+			console.log(recipeResponse)
 
 
-// this code access the cooking instruction and loops through the length of the instruction array		
-let instruction = ""
+			// this code access the cooking instruction and loops through the length of the instruction array		
+			let instruction = ""
 
- for (let index = 0; index < recipeResponse.results[0].instructions.length; index++) {
+			for (let index = 0; index < recipeResponse.results[0].instructions.length; index++) {
 
-// this code adds the step number to each instruction and puts each step on a newline	
-	instruction += 'Step ' +  (index+1) +' - ' + recipeResponse.results[0].instructions[index].display_text +'\n'
- }
-console.log(instruction)
+				// this code adds the step number to each instruction and puts each step on a newline	
+				instruction += 'Step ' + (index + 1) + ' - ' + recipeResponse.results[0].instructions[index].display_text + '\n'
+			}
+			console.log(instruction)
 
- // this code accesses the recipe image from the API
-let recipeImage = recipeResponse.results[0].thumbnail_url;
-console.log (recipeImage)
+			// this code accesses the recipe image from the API
+			let recipeImage = recipeResponse.results[0].thumbnail_url;
+			console.log(recipeImage)
 
-// this code accesses the number of servings
-let servingSize = recipeResponse.results[0].num_servings;
-console.log("number of servings = " + servingSize)
+			// this code accesses the number of servings
+			let servingSize = recipeResponse.results[0].num_servings;
+			console.log("number of servings = " + servingSize)
 
-// this code accesses the cooking time (some seem to have this field missing and return null)
-let recipeMaxTime = recipeResponse.results[0].total_time_minutes
-console.log(recipeMaxTime)
+			// this code accesses the cooking time (some seem to have this field missing and return null)
+			let recipeMaxTime = recipeResponse.results[0].total_time_minutes
+			console.log(recipeMaxTime)
 
+
+		})
+
+})
+/////////////////////END OF RECIPE SEARCH CODE SECTION //////////////////////////////////////////////
+
+
+/////////////// THE BELOW CODE IS FOR THE RANDOM RECIPE GENERATOR AND IS WIP///////////////
+
+
+// This Code returns a random recipe when a button is pressed
+
+let randomRecipeBtn = document.querySelector("#random-button");
+// this function captures the inputted recipe name and stores it in a variable called recipe name
+
+randomRecipeBtn.addEventListener("click", function (event) {
+	event.preventDefault();
+
+	function getRandomObjectFromArray(array) {
+		let randomIndex = Math.floor(Math.random() * array.length);
+		let randomObject = array[randomIndex];
+		return randomObject;
+	}
+
+	let cookingIngredients = [
+		"apple",
+		"sugar",
+		"oranges",
+		"eggs",
+		"tea",
+		"butter",
+		"milk",
+		"eggs",
+		"vanilla",
+		"chocolate",
+		"rosemary",
+		"sage",
+		"bread",
+		"olives",
+		"garlic",
+		"onion",
+		"beef",
+		"tomatoes",
+		"prawns",
+		"oregano",
+		"basil",
+		"chicken",
+		"cod",
+		"rice",
+		"beans",
+		"salsa",
+		"cheddar",
+		"lettuce",
+		"tortilla",
+		"avocado",
+		"cream"
+	];
+
+	let randomIngredient = getRandomObjectFromArray(cookingIngredients);
+	console.log(randomIngredient);
+
+	// this code request a random recipe from the tasty API 
+	fetch('https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=' + randomIngredient, options)
+		.then(response => response.json())
+		.then(function (randomRecipeResponse) {
+			console.log(randomRecipeResponse)
+
+			// this code console logs the random recipe name		
+			let randomRecipeName = randomRecipeResponse.results[0].display;
+			// console.log(randomRecipeName);
+
+
+			// this code request the random recipe from the tasty API that matches the inputed variable called recipeName	
+			fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=1&q=' + randomRecipeName, options)
+				.then(response => response.json())
+				.then(function (recipeResponse) {
+					console.log(recipeResponse)
+
+					// this code console logs the description of the random recipe
+
+					let randomRecipeDescription = "";
+					randomRecipeDescription = recipeResponse.results[0].description;
+					console.log(randomRecipeDescription);
+
+
+					// this code access the cooking instruction fof the random recipe and loops through the length of the instruction array		
+					let instruction = ""
+
+					for (let index = 0; index < recipeResponse.results[0].instructions.length; index++) {
+
+						// this code adds the step number to each instruction and puts each step on a newline	
+						instruction += 'Step ' + (index + 1) + ' - ' + recipeResponse.results[0].instructions[index].display_text + '\n'
+					}
+					console.log(instruction)
+
+					// this code accesses the random recipe image from the API
+					let recipeImage = recipeResponse.results[0].thumbnail_url;
+					console.log(recipeImage)
+
+					// this code accesses the number of servings for the random recipe
+					let servingSize = recipeResponse.results[0].num_servings;
+					console.log("number of servings = " + servingSize)
+
+					// this code accesses the cooking time for the random recipe(some seem to have this field missing and return null)
+					let recipeMaxTime = recipeResponse.results[0].total_time_minutes
+					console.log(recipeMaxTime)
+
+
+				})
+
+		})
 
 })
 
-})
-
-// ('https://tasty.p.rapidapi.com/recipes/list?from=0&size=1&q=carbonara', options)
-// this code searches the recipe API for the name of the recipe stored in the recipeName variable
-
-
-
-// this code returns the recipe instruction of how to make the recipe
+///////////////////// END of CODE FOR THE RANDOM RECIPE GENERATOR ///////////////////////////
